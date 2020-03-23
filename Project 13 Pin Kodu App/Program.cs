f91e576b07e3fb2eab9edb8f6b25bad9da944227
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 04:20:02 +0300

    Initial commit

---
+++ Project 13 Pin Kodu App/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_13_Pin_Kodu_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pin = "12345";
+            string UPin;
+
+            int hak = 3;
+            int i = 0;
+
+            while (hak>i)
+            {
+                Console.Write("Pin Değerini Giriniz:");
+                UPin = Console.ReadLine();
+
+                if (UPin==pin)
+                {
+                    Console.WriteLine("Başarılı Bir Şekilde Giriş Yapıldı");
+                    break;
+                }
+
+                else
+                {
+                    hak--;
+                    Console.WriteLine("Yanlış Giriş Yaptınız Kalan Hakkınız:{0}",hak);
+                }
+            }
+
+            if (hak==0)
+            {
+                Console.WriteLine("Sim Kartınız Bloke Edildi");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Console_52_Pin_Kodu_App_v.2.0
+ 
